db0dd24d65872146b8d83de6d1e85b4c93df106d
jakeschurch <jschurch@bryant.edu>
2017-07-29 15:07:50 -0400

    Added solution files to repository

---
+++ CSharp-Algorithms/Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static List<int> AlgoList = new List < int > {};
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
